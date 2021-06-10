b05010105a0a65e7061104c514cd295b0b475c2f
Shadman Saif Anonno <shadmansaif@outlook.com>
2021-06-10 23:56:11 +0600

    Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,77 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+// Components
+import About from "./components/About";
 import Timer from "./components/Timer";
 export class App extends Component {
   render() {
     return (
       <div>
-        <Timer></Timer>
+        <Router>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Link className="nav-link" to="/timer">
+                  Timer
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form inline>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
+          <Switch>
+            <Route path="/about">
+              <About></About>
+            </Route>
+            <Route path="/timer">
+              <Timer></Timer>
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
 }
 
 export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
