3982423a2149e069c2a8e2dce625594bd9041343
Shadman Saif Anonno <shadmansaif@outlook.com>
2021-06-29 23:21:40 +0600

    onSubmit

--- final/src/views/Register.js
+++ final/src/views/Register.js
@@ -1,37 +1,56 @@
-import React from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 function Register() {
-    return (
-        // <Container fluid>
-        <Container className="pt-4">
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log("triggered");
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-            <div>
-                <p>Other options</p>
-            </div>
-        </Container>
-    )
+    let myForm = document.getElementById("myForm");
+    let formData = new FormData(myForm);
+    // console.log(formData.getAll('password'));
+    // console.log(formData.keys());
+    for (var key of formData.keys()) {
+      console.log(key, formData.getAll(key));
+    }
+  };
+
+  
+
+  return (
+    // <Container fluid>
+    <Container className="pt-4">
+      <Form id="myForm" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control name="email" type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <div>
+        <p>Other options</p>
+      </div>
+    </Container>
+  );
 }
 
-export default Register
+export default Register;
