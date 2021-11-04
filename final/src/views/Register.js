09dd56816f02f7c1d7948b7526171c35fbcf1600
Shadman Saif Anonno <shadmansaif@outlook.com>
2021-11-05 00:30:54 +0600

    register page in progress

--- final/src/views/Register.js
+++ final/src/views/Register.js
@@ -6,13 +6,37 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 
-const Link = require("react-router-dom").Link
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const Link = require("react-router-dom").Link;
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    // margin: theme.spacing(1),
+    minWidth: 120,
+    width: "100%",
+  },
+  optionCol: {
+    color: "orange",
+  },
+}));
 
 function Register() {
+  const classes = useStyles();
   // define state
   const [user, setUser] = useState({
     email: "",
     password: "",
+    password_confirm: "",
+    firstname: "",
+    lastname: "",
+    type: "",
   });
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
@@ -31,6 +55,7 @@ function Register() {
   };
 
   const handleChange = (event) => {
+    // alert("Change")
     // this.setState({value: event.target.value});
     // When copied using the spread operator, the type of copy is actually a deep copy
     let newUser = {
@@ -56,27 +81,66 @@ function Register() {
       className="pt-4"
     >
       <Form id="myForm" onSubmit={handleSubmit}>
+        <div className="row">
+          {/* Email */}
+          <div className="col-md-6">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <TextField
+                placeholder="Enter email"
+                name="email"
+                // value={email}
+                value={user.email}
+                onChange={handleChange}
+                label="Email"
+                variant="outlined"
+                style={{
+                  width: "100%",
+                }}
+              />
+            </Form.Group>
+          </div>
+          {/* Email */}
+
+          {/* First Name */}
+          <div className="col-md-6">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <TextField
+                placeholder="Enter first name"
+                name="firstname"
+                // value={email}
+                value={user.firstname}
+                onChange={handleChange}
+                label="First Name"
+                variant="outlined"
+                style={{
+                  width: "100%",
+                }}
+              />
+            </Form.Group>
+          </div>
+          {/*First Name */}
+        </div>
+        {/* Last Name */}
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <TextField
-            placeholder="Enter email"
-            name="email"
+            placeholder="Enter last name"
+            name="lastname"
             // value={email}
-            value={user.email}
+            value={user.lastname}
             onChange={handleChange}
-            label="Email"
+            label="Last Name"
             variant="outlined"
             style={{
               width: "100%",
             }}
           />
-          {/* <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text> */}
         </Form.Group>
+        {/* Last Name */}
 
+        {/* Password */}
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <TextField
-            placeholder="Enter email"
+            placeholder="Enter password"
             name="password"
             // value={password}
             value={user.password}
@@ -89,9 +153,49 @@ function Register() {
             }}
           />
         </Form.Group>
-        {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group> */}
+        {/* Password */}
+
+        {/* Password */}
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <TextField
+            placeholder="Confirm password"
+            name="password_confirm"
+            // value={password}
+            value={user.password_confirm}
+            onChange={handleChange}
+            label="Confirm Password"
+            variant="outlined"
+            type="password"
+            style={{
+              width: "100%",
+            }}
+          />
+        </Form.Group>
+        {/* Password */}
+
+        {/* Type */}
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel id="demo-simple-select-outlined-label">Type</InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={user.type}
+            onChange={handleChange}
+            label="Type"
+            name="type"
+          >
+            <MenuItem value="uedviuewy">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem className={classes.optionCol} value={10}>
+              Ten
+            </MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        {/* Type */}
+
         <div className="text-center">
           {/* <Button variant="primary" type="submit">
             Submit
